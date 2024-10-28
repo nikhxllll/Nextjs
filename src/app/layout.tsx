de5fff8ee0945a7c4ef0c9aef841696383a8ea84
Nikhil Yadav <ny619619@gmail.com>
2024-10-28 12:36:40 +0530

    changes made to understand next js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <h1 className="text-4xl text-center">nav Items</h1>
         {children}
       </body>
     </html>
